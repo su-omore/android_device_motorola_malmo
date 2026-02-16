23d00ad4f3dfe3951b8b098954df1de6af82588c
su-omore <su.configg@gmail.com>
2026-02-16 08:10:22 +0300

    Change lunch combo from omni_malmo to twrp_malmo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_malmo-user
-add_lunch_combo omni_malmo-userdebug
-add_lunch_combo omni_malmo-eng
+add_lunch_combo twrp_malmo-user
+add_lunch_combo twrp_malmo-userdebug
+add_lunch_combo twrp_malmo-eng
