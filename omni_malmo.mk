9d88fed3312e13ac5cef6536183fc24cea6aa2b2
su-omore <su.configg@gmail.com>
2026-02-16 06:00:17 +0300

    Update twrp_malmo.mk

--- omni_malmo.mk
+++ omni_malmo.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from malmo device
 $(call inherit-product, device/motorola/malmo/device.mk)
 
 PRODUCT_DEVICE := malmo
-PRODUCT_NAME := omni_malmo
+PRODUCT_NAME := twrp_malmo
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g85 5G
 PRODUCT_MANUFACTURER := motorola
