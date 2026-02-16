0e44358f7846cd668fc168ab95aab05be8338236
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-02-16 04:46:01 +0100

    malmo: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g85 5G Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_malmo.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_malmo-user \
+    omni_malmo-userdebug \
+    omni_malmo-eng
